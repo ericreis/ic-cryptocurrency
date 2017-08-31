bb4ebb1c6a71d4570678a52cd9c7fa960b59e39d
Eric Reis Figueiredo <eric_reisfig@poli.ufrj.br>
2017-08-31 20:00:45 -0300

    first commit

---
+++ correlation_matrix.py
@@ -0,0 +1,33 @@
+print(__doc__)
+
+import sys
+import ntpath
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+
+# get dataset info from arguments
+dataset_path = sys.argv[1]
+dataset_name = ntpath.basename(dataset_path).split(".")[0]
+
+print(dataset_path)
+print(dataset_name)
+
+# import dataset as panda dataframe
+df = pd.DataFrame.from_csv(dataset_path)
+
+# rename columns
+df.rename(columns=lambda x: "x" + str(np.where(df.columns == x)[0][0]), inplace=True)
+
+corr = df.corr()
+plt.figure(figsize=(12,12))
+plt.matshow(corr, fignum=1)
+plt.xticks(range(len(corr.columns)), corr.columns);
+plt.yticks(range(len(corr.columns)), corr.columns);
+plt.clim(-1, 1)
+plt.colorbar()
+
+plt.savefig("plots/correlation-matrix/" + dataset_name + ".png")
+
+plt.show()
---
+++ scatter_matrix.py
@@ -0,0 +1,29 @@
+print(__doc__)
+
+import sys
+import ntpath
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+
+
+# get dataset info from arguments
+dataset_path = sys.argv[1]
+dataset_name = ntpath.basename(dataset_path).split(".")[0]
+
+print(dataset_path)
+print(dataset_name)
+
+# import dataset as panda dataframe
+df = pd.DataFrame.from_csv(dataset_path)
+
+# rename columns
+df.rename(columns=lambda x: "x" + str(np.where(df.columns == x)[0][0]), inplace=True)
+
+# create the scatter matrix matplotlib object
+pd.plotting.scatter_matrix(df, alpha=0.2, figsize=(12, 12), diagonal='kde')
+
+# save plot and show
+plt.savefig("plots/scatter-matrix/" + dataset_name + ".png")
+plt.show()
